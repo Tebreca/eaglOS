360c6fa7a0e970063aecc43a62185a2f79417d48
Tebreca <ewan.arends10@gmail.com>
2017-06-30 17:45:57 +0200

    update 1
    updated backgroud, now working an a start menu

--- eaglOS/public_html/users/ewan/script/desktop.js
+++ eaglOS/public_html/users/ewan/script/desktop.js
@@ -1,4 +1,22 @@
 var bar = document.getElementById("bar");
+var menu = document.getElementById("menu");
+var isvisible = false;
+document.getElementById("menu").style.visibility = "hidden";
 bar.addEventListener('click', function(){
-
+      if (isvisible === false){
+    document.getElementById("menu").style.visibility = "visible";
+    isvisible = true;
+    } else{
+      document.getElementById("menu").style.visibility = "hidden"; 
+      isvisible = false;
+    }
+}); 
+menu.addEventListener('click', function(){
+    if (isvisible === false){
+    document.getElementById("menu").style.visibility = "visible";
+    isvisible = true;
+    } else{
+      document.getElementById("menu").style.visibility = "hidden"; 
+      isvisible = false;
+    }
 });
